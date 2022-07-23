8c3c4a2f78d5c035573da937705bd8ad0ee229f5
Asish <luckyasish13@gmail.com>
2022-07-23 14:57:30 +0530

    added util function to generate random instance with time windows

--- utils.py
+++ utils.py
@@ -101,7 +101,7 @@ def compare_solvers(n, m, cost, xc, yc, **params):
 
 def random_routing_instance(n, seed=None):
     """
-    Generate a random TSP instance (n+1 random coordinates and a cost matrix which is same as the distance between them).
+    Generate a random instance (n+1 random coordinates and a cost matrix which is same as the distance between them).
     Args:
         n: No. of nodes exclusing depot.
         seed: Seed value for random number generator. Defaults to None, which sets a random seed.
@@ -114,7 +114,7 @@ def random_routing_instance(n, seed=None):
     if seed is not None:
         np.random.seed(seed)
 
-    # Generate TSP distance_matrix
+    # Generate distance_matrix
     xc = (np.random.rand(n + 1) - 0.5) * 20
     yc = (np.random.rand(n + 1) - 0.5) * 20
     xc[0], yc[0] = 0, 0
@@ -125,7 +125,46 @@ def random_routing_instance(n, seed=None):
             dist_mat[j, i] = dist_mat[i, j]
 
     # Return output
-    return dist_mat.astype(int), xc, yc
+    return xc, yc, dist_mat.astype(int)
+
+
+def random_routing_instance_with_time_windows(n, seed=None):
+    """
+    Generate a random instance (n+1 random coordinates and a cost matrix which is same as the distance between them).
+    Args:
+        n: No. of nodes exclusing depot.
+        seed: Seed value for random number generator. Defaults to None, which sets a random seed.
+    Returns:
+        x: a list of (n + 1) x coordinates,
+        y: a list of (n + 1) y coordinates,
+        cost: an (n + 1) x (n + 1) numpy array as the cost matrix,
+        tw: a list of pair of numbers as the time windows for each coordinate
+    """
+    
+    # Set seed
+    if seed is not None:
+        np.random.seed(seed)
+
+    # Generate distance_matrix
+    xc = (np.random.rand(n + 1) - 0.5) * 20
+    yc = (np.random.rand(n + 1) - 0.5) * 20
+    xc[0], yc[0] = 0, 0
+    dist_mat = np.zeros((n + 1, n + 1))
+    for i in range(n + 1):
+        for j in range(i + 1, n + 1):
+            dist_mat[i, j] = np.sqrt((xc[i] - xc[j]) ** 2 + (yc[i] - yc[j]) ** 2) * 100
+            dist_mat[j, i] = dist_mat[i, j]
+
+    MAX_EARLIEST_ARRIVAL_TIME = 5   # The maximum possible "earliest arrival time"
+    MAX_TIME_WINDOW_RANGE = 30      # The largest possible time window size
+
+    time_windows = [[0, 30]]
+    for node in range(n):
+        ti = 0  # round(np.random.rand() * MAX_EARLIEST_ARRIVAL_TIME)
+        tf = 30 # round(np.random.rand() * MAX_TIME_WINDOW_RANGE + ti)
+        time_windows.append([ti, tf])
+
+    return xc, yc, dist_mat.astype(int), time_windows
 
 
 def custom_routing_instance(n):
