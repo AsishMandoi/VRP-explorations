6e3fefb042a33a0461645f6ae39dec24d31632da
Asish <luckyasish13@gmail.com>
2022-08-06 18:52:37 +0530

    updated solve_vrp.py

--- solve_vrp.py
+++ solve_vrp.py
@@ -1,12 +1,22 @@
+import numpy as np
 from VRP.classical.ras import RAS as ExactSolver
 from VRP.quantum.CQM_based.fqs_v2 import FQS
-import utils
 
 n=7     # number of clients
 m=3     # number of vehicles
 
 def vrp_solver(n, m, **params):
-  xc, yc, cost = utils.random_routing_instance(n, 0)
+  np.random.seed(0)
+  xc = (np.random.rand(n + 1) - 0.5) * 20
+  yc = (np.random.rand(n + 1) - 0.5) * 20
+  xc[0], yc[0] = 0, 0
+  cost = np.zeros((n + 1, n + 1))
+  for i in range(n + 1):
+      for j in range(i + 1, n + 1):
+          cost[i, j] = np.sqrt((xc[i] - xc[j]) ** 2 + (yc[i] - yc[j]) ** 2) * 100
+          cost[j, i] = cost[i, j]
+
+  cost = cost.astype(int)
 
   c_solver = ExactSolver(n, m, cost, xc, yc)
   c_sol = c_solver.formulate_and_solve()
